03148d5344b33b5ad8861ac0baf5215211ad6904
Alex Biczak <biczak@Athena.local>
2017-08-15 20:55:36 -0400

    Fixed Bugs at High Resolutions

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -14,18 +14,20 @@ const Carousel = (props) => {
     slidesToScroll: props.slidesToScroll,
   };
   return (
-    <div className="Hero_Panel">
-      <Slider {...settings}>
-        <img className="sliderImage" src="../assets/carousel1.png" alt="carousel1" />
-        <img className="sliderImage" src="../assets/carousel2.png" alt="carousel2" />
-        <img className="sliderImage" src="../assets/carousel3.png" alt="carousel3" />
-      </Slider>
-      <p className="carousel-header">Screening <br /> DVF Secret Agent Part 2 <br /></p>
-      <p className="carousel-subHeader1">October 15, 2017</p>
-      <p className="carousel-subHeader2">Join us for a private screening of DVF Secret Agent Part 2
-        <br />
-        with our special guest, director Peter Lindburgh
-      </p>
+    <div className="carousel-container">
+      <div className="Hero_Panel">
+        <Slider {...settings}>
+          <img className="sliderImage" src="../assets/carousel1.png" alt="carousel1" />
+          <img className="sliderImage" src="../assets/carousel2.png" alt="carousel2" />
+          <img className="sliderImage" src="../assets/carousel3.png" alt="carousel3" />
+        </Slider>
+        <p className="carousel-header">Screening <br /> DVF Secret Agent Part 2 <br /></p>
+        <p className="carousel-subHeader1">October 15, 2017</p>
+        <p className="carousel-subHeader2">Join us for a private screening of DVF Secret Agent Part 2
+          <br />
+          with our special guest, director Peter Lindburgh
+        </p>
+      </div>
     </div>
   );
 };
